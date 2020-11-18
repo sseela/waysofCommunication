c065134d24a689f4b7e72e0b780ab5e0f1501845
sseela <s.sabelli@bstore.capgemini.com>
2020-11-18 16:18:49 +0530

    Revert "TestCommit"
    This reverts commit 69cf981e49c150955df2bb629d680667fbd04bb0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
-import { SiblingComponent } from './sibling/sibling.component';
-import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ParentComponent,
-    ChildComponent,
-    SiblingComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
